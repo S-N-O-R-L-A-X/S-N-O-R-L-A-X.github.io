773e14573cc696312dd00b512d8a58dd5b1c8b06
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-23 22:42:26 +0800

    feat: add sentences

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,11 @@
 import Quote from "../../components/Quote/Quote";
-
+import Profile from "../../components/Profile/Profile";
 export default function Home() {
   return (
     <div>
       <div className="quote"><Quote /></div>
+      <div>cards</div>
+      <div><Profile /></div>
     </div>
   )
 }
