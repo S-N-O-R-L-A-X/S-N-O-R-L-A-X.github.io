7a684039e2047846589ed8065116cbdbfef407c1
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-23 20:13:30 +0800

    feat: add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import Octocat from "./octocat";
+
+export default function Header() {
+  return (
+    <div>
+      Welcome to my site.
+      <Octocat />
+    </div>
+  )
+}
--- src/views/Layout/Layout.tsx
+++ src/views/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import "./index.css"
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Header from "../../components/Header/Header";
 export default function Layout() {
   return (
     <div>
       <div className="aside"><Sidebar /></div>
-      <div className="header">snorlax's blog</div>
+      <div className="header"><Header /></div>
       <div className="content">content</div>
     </div>
   );
