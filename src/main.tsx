2bf44fd58ab5de3aad10474a5125a1f984c1cbc3
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-24 23:03:27 +0800

    refactor: remove router

--- src/main.tsx
+++ src/main.tsx
@@ -2,31 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-import ErrorPage from './views/ErrorPage/ErrorPage';
 import Layout from './views/Layout/Layout';
-import Home from './views/Home/Home';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "home",
-        element: <Home />
-      }
-    ]
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Layout />
   </React.StrictMode>,
 )
--- src/views/Layout/Layout.tsx
+++ src/views/Layout/Layout.tsx
@@ -1,13 +1,14 @@
 import "./index.css"
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Header from "../../components/Header/Header";
-import { Outlet } from "react-router-dom";
+import Home from "../Home/Home";
+
 export default function Layout() {
   return (
     <div>
       <div className="aside"><Sidebar /></div>
       <div className="header"><Header /></div>
-      <div className="content"><Outlet></Outlet></div>
+      <div className="content"><Home /></div>
     </div>
   );
 }
