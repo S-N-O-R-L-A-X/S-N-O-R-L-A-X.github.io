a5a6a7658eba61cd5f000201313e68dac219ad47
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-24 22:30:16 +0800

    feat: refine sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,39 +1,44 @@
 import { SideNavigation, SideNavigationItem, SideNavigationSubItem } from '@ui5/webcomponents-react';
+import { Ui5CustomEvent, SideNavigationDomRef } from '@ui5/webcomponents-react';
 
 import "./index.css";
 
+
 export default function Sidebar() {
+  function navigate(event: Ui5CustomEvent<SideNavigationDomRef, { item: HTMLElement; }>) {
+    console.log((event.detail.item as any)["_state"].text);
+
+  }
   return (
     <div className="sidebar">
       <SideNavigation
         style={{ width: "100%" }}
-        fixedItems={<><SideNavigationItem icon="chain-link" text="Useful Links" /><SideNavigationItem icon="history" text="History" /></>}
-        onSelectionChange={function Ta() {}}
+        fixedItems={<><SideNavigationItem icon="chain-link" text="Useful Links" />
+          <SideNavigationItem icon="history" text="History" /></>}
+        onSelectionChange={navigate}
       >
         <SideNavigationItem
           icon="home"
           text="Home"
         />
         <SideNavigationItem
+          icon="group"
+          text="Other projects"
+        >
+          <SideNavigationSubItem text="noname extension" />
+          <SideNavigationSubItem text="Bridge" />
+          <SideNavigationSubItem text="leetcode" />
+          <SideNavigationSubItem text="tricks in javascript" />
+        </SideNavigationItem>
+
+        <SideNavigationItem
           expanded
           icon="group"
-          text="People"
+          text="Daily"
         >
-          <SideNavigationSubItem text="From My Team" />
-          <SideNavigationSubItem text="From Other Teams" />
+          <SideNavigationSubItem text="2023" />
         </SideNavigationItem>
-        <SideNavigationItem
-          icon="locate-me"
-          selected
-          text="Locations"
-        />
-        <SideNavigationItem
-          icon="calendar"
-          text="Events"
-        >
-          <SideNavigationSubItem text="Local" />
-          <SideNavigationSubItem text="Others" />
-        </SideNavigationItem>
+
       </SideNavigation>
 
     </div>
