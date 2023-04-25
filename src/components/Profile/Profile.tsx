81fa8cc68db8835649c7f482da57e484539b0e38
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-25 23:15:27 +0800

    feat: add personal info

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,16 @@
 import Label from "../Label/Label";
 
+import { Icon } from "@ui5/webcomponents-react";
+import "@ui5/webcomponents-icons/dist/email";
+import github_mark from "../../assets/github-mark.png";
 
 export default function Profile() {
   return (
     <>
+      <Icon name="email"></Icon>snorlax.xu@outlook.com
+      <br />
+      <img src={github_mark} style={{ width: "1rem", height: "1rem" }} />S-N-O-R-L-A-X
+      <br />
       <Label>桥牌</Label>
       <Label>足球</Label>
       <Label>篮球</Label>
