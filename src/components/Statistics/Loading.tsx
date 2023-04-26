25b46c1f0e1fcfd52e294a0e0e6c907b24a94310
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-26 16:38:03 +0800

    feat: add loading

---
+++ src/components/Statistics/Loading.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+export default function Loading() {
+  return (
+    <div className="loading" >
+      <div>G</div>
+      <div>N</div>
+      <div>I</div>
+      <div>D</div>
+      <div>A</div>
+      <div>O</div>
+      <div>L</div>
+    </div >
+  )
+}
--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,40 +1,60 @@
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import "./index.css";
+import Loading from "./Loading";
 
 export default function Statistics() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isError, setIsError] = useState<boolean>(false);
-  function handleError(e: SyntheticEvent) {
-    setIsError(true);
-    console.log("enter");
-    console.log(e);
+  const card_links = ["https://github-readme-streak-stats.herokuapp.com/?user=S-N-O-R-L-A-X&theme=dark&hide_border=true", "https://github-readme-stats.vercel.app/api?username=S-N-O-R-L-A-X&hide_title=true&hide_border=true&show_icons=trueline_height=21&text_color=000&icon_color=000&bg_color=0,ea6161,ffc64d,fffc4d,52fa5a&theme=graywhite"];
+  const images: HTMLImageElement[] = [];
+  function getPictures() {
+    card_links.forEach((card_link) => {
+      const image = new Image();
+      image.src = card_link;
+      image.onload = () => {
+        images.push(image);
+      }
+      image.onerror = () => {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    })
   }
+
+  function handleError(e: SyntheticEvent) {
+    setIsLoading(false);
+    setIsError(true);
+  }
+
+  getPictures();
 
   return (
     <>
-      {isError ?
-        (
-
-          <div className="error-box">
-            <div className="face2">
-              <div className="eye"></div>
-              <div className="eye right"></div>
-              <div className="mouth sad"></div>
+      {
+        isLoading ? <Loading /> : (
+          isError ? (
+            <div className="error-box">
+              <div className="face2">
+                <div className="eye"></div>
+                <div className="eye right"></div>
+                <div className="mouth sad"></div>
+              </div>
+              <div className="shadow move"></div>
+              <div className="message">
+                <p className="alert">Oops!</p>
+                <p className="info">Statistics should be here, but it seems that your network is not good enough. Try using VPN if you are in China.</p>
+              </div>
+              {/* <button className="button-box"><h1 className="red">try again</h1></button> */}
             </div>
-            <div className="shadow move"></div>
-            <div className="message">
-              <p className="alert">Oops!</p>
-              <p className="info">Statistics should be here, but it seems that your network is not good enough. Try using VPN if you are in China.</p>
-            </div>
-            {/* <button className="button-box"><h1 className="red">try again</h1></button> */}
-          </div>
+          ) : (
+            <div onError={handleError} onLoad={e => setIsLoading(false)} className="statistics">
 
-        ) :
-        (
-          <div onError={handleError} className="statistics">
-            <img onError={handleError} src="https://github-readme-streak-stats.herokuapp.com/?user=S-N-O-R-L-A-X&theme=dark&hide_border=true" />
-            <img onError={handleError} src="https://github-readme-stats.vercel.app/api?username=S-N-O-R-L-A-X&hide_title=true&hide_border=true&show_icons=trueline_height=21&text_color=000&icon_color=000&bg_color=0,ea6161,ffc64d,fffc4d,52fa5a&theme=graywhite"></img>
-          </div>
+              <img onError={handleError} src={images[0].src} />
+              <img onError={handleError} src={images[1].src}></img>
+            </div>
+          )
         )
+
       }
 
 
