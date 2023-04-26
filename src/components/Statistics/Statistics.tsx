0f926ddcc029f753745201a594e3447c6c9f8d58
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-26 12:57:16 +0800

    feat: add statistics

---
+++ src/components/Statistics/Statistics.tsx
@@ -0,0 +1,11 @@
+import "./index.css"
+export default function Statistics() {
+  return (
+    <>
+      <div className="statistics">
+        <img src="https://github-readme-streak-stats.herokuapp.com/?user=S-N-O-R-L-A-X&theme=dark&hide_border=true" />
+        <img src="https://github-readme-stats.vercel.app/api?username=S-N-O-R-L-A-X&hide_title=true&hide_border=true&show_icons=trueline_height=21&text_color=000&icon_color=000&bg_color=0,ea6161,ffc64d,fffc4d,52fa5a&theme=graywhite"></img>
+      </div>
+    </>
+  )
+}
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,7 @@
 import Quote from "../../components/Quote/Quote";
 import Profile from "../../components/Profile/Profile";
+import Statistics from "../../components/Statistics/Statistics";
+
 import "./index.css"
 export default function Home() {
   return (
@@ -7,10 +9,7 @@ export default function Home() {
       <div className="quote"><Quote /></div>
       <div className="wrap">
         <div className="cards">
-          I wanted to put some statistics here, but I failed.
-
-          {/* <img src="https://komarev.com/ghpvc/?username=S-N-O-R-L-A-X" />
-          <img src="https://github-readme-stats.vercel.app/api?username=S-N-O-R-L-A-X&include_all_commits=true&count_private-true&custom_title=你的账号用户名'%20GitHub%20Stats&line_height=30&show_icons=true&hide_border=true&bg_color=192133&title_color=efb752&icon_color=efb752&text_color=70bed9" /> */}
+          <Statistics />
         </div>
         <div className="profile"><Profile /></div>
       </div>
