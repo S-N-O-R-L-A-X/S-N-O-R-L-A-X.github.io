a6714ecbd85a9b07c4dea74f9791431cb9c5e599
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-04-26 13:20:07 +0800

    feat: reuse react router

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,20 @@ import './index.css'
 
 import Layout from './views/Layout/Layout';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
